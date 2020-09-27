8dc829f4d3edc63574a716abf7492fcaf071778b
Victor Carity <victorcarity@gmail.com>
2020-09-27 20:47:25 -0300

    First Test

--- database/factory.js
+++ database/factory.js
@@ -19,3 +19,14 @@
 //     username: faker.username()
 //   }
 // })
+
+const Factory = use('Factory')
+const Hash = use('Hash')
+
+Factory.blueprint('App/Models/User', async (faker) => {
+  return {
+    username: faker.username(),
+    email: faker.email(),
+    password: await Hash.make(faker.password())
+  }
+})
---
+++ test/functional/store-user.spec.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const Factory = use('Factory')
+const User = use('App/Models/User')
+const { test, trait } = use('Test/Suite')('Store User')
+
+trait('Test/ApiClient')
+
+test('Store a new user and generates a jwt', async ({ assert, client }) => {
+  const { username, email, password } = await Factory.model('App/Models/User').make()
+
+  const response = await client.post('/api/register')
+    .send({
+      username,
+      email,
+      password
+    })
+    .end()
+
+  response.assertStatus(200)
+
+  response.assertJSONSubset({
+    user: {
+      email,
+      username
+    }
+  })
+
+  assert.isDefined(response.body.token)
+
+  await User
+    .query()
+    .where({ email })
+    .firstOrFail()
+})
