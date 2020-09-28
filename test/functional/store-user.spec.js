ac7493a257292bfc8394cbd444c6a25e18629fac
Victor Carity <victorcarity@gmail.com>
2020-09-27 22:20:24 -0300

    Test empty field.

--- test/functional/store-user.spec.js
+++ test/functional/store-user.spec.js
@@ -6,17 +6,17 @@ const { test, trait } = use('Test/Suite')('Store User')
 
 trait('Test/ApiClient')
 
-test('Store a new user and generates a jwt', async ({ assert, client }) => {
+test('store a new user and generates a jwt', async ({ assert, client }) => {
   const { username, email, password } = await Factory.model('App/Models/User')
     .make()
 
   const response = await client.post('/api/store')
-    .send({
-      username,
-      email,
-      password
-    })
-    .end()
+  .send({
+    username,
+    email,
+    password
+  })
+  .end()
 
   response.assertStatus(200)
 
@@ -36,7 +36,7 @@ test('Store a new user and generates a jwt', async ({ assert, client }) => {
 })
 
 
-test("Returns an error if user already exists", async ({ assert, client }) => {
+test("returns an error if user already exists", async ({ assert, client }) => {
   const { username, email, password } = await Factory.model('App/Models/User')
     .create()
 
@@ -47,6 +47,20 @@ test("Returns an error if user already exists", async ({ assert, client }) => {
     password
   })
   .end()
+
+  response.assertStatus(422)
+
+  assert.isUndefined(response.body.token)
+})
+
+test('returns an error if username is not provided', async ({ assert, client }) => {
+  const { email, password } = await Factory.model('App/Models/User')
+    .make()
+
+  const response = await client
+    .post('/api/store')
+    .send({ email, password })
+    .end()
 
   response.assertStatus(422)
 
